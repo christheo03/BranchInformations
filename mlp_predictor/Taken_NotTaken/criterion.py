dc0ac5d4291a7df68070b6225157b97cefc7dcfd
christheo03 <christos99077019@gmail.com>
2026-07-06 12:50:13 +0300

    Update loss calc using bce

--- mlp_predictor/Taken_NotTaken/criterion.py
+++ mlp_predictor/Taken_NotTaken/criterion.py
@@ -1,18 +1,11 @@
-from ml_configs import nn, torch
-
+from ml_configs import torch,nn
 
 class DynamicMissPredictionLoss(nn.Module):
     def __init__(self):
-        super(DynamicMissPredictionLoss,self).__init__()
+        super(DynamicMissPredictionLoss, self).__init__()
     
-    def forward(self,outputs,targets,weights,rates):
+    def forward(self, outputs, targets, weights, rates):
         y_k = outputs.squeeze(-1) if outputs.dim() > 1 else outputs
-
-        emb = (1.0 - y_k) * rates * weights
-        bit =  y_k*(1.0 - rates) * weights
-
-        return  torch.sum(emb+bit) / torch.sum(weights)
-
-        
-        
-
+        bce = torch.nn.functional.binary_cross_entropy(y_k, targets, reduction='none') # Loss from the actual target compares to the predicted
+        sample_weights = weights * torch.abs(2.0 * rates - 1.0) 
+        return torch.sum(bce * sample_weights) / torch.sum(weights)
